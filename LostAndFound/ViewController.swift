dfaaa65bfdb16d29b8b830ddad170ef4798a66d2
eileenyoo2 <eileenyoo2@gmail.com>
2020-10-02 13:30:28 +0900

    alamofire 버전 업데이트 / string으로 받아오던 것 enum 으로 변경중

--- LostAndFound/ViewController.swift
+++ LostAndFound/ViewController.swift
@@ -107,6 +107,7 @@ class ViewController: UIViewController {
     
     func sendRequest(completeHandler:@escaping (JSON) -> (),failureHandler:@escaping (String) -> ()) {
         
+        // endIndex 숫자 변경하기
         let url = APIDefine.getLostArticleAPIAddress(startIndex: 0, endIndex: 5, type: LostArticleType.getEnumFromKoreanType(korean: (articleTextField?.text)!), place: LostPlaceType.getEnumFromKoreanType(korean: (placeTextField?.text)!), searchTxt: nil)
         
         // 정보를 불러오기만 하는 것이므로 get 방식 사용
@@ -119,24 +120,8 @@ class ViewController: UIViewController {
             // 통신 성공
             case .success(let value):   // 주의할 점 : String 타입으로 들어오는걸 Data 타입으로 바꿔줘야한다.
                 let json = JSON.init(value.data(using: .utf8) as Any)
-//                let resultJson = json["SearchLostArticleService"]["row"][0]["TAKE_PLACE"].stringValue
-//                print("JSON: \(resultJson)")
                 completeHandler(json)
-                // 질문해할 것 -> 개별적인 이름으로 순서대로 저장하고 있어야하는건지 or 딕셔너리 형태로 값을 갖고있어야하는건지
-                
-                
-//                for i in 0..<json["SearchLostArticleService"]["row"].count {
-//                    var item:LostArticleResult = LostArticleResult()
-//                    item.id = json["SearchLostArticleService"]["row"][i]["ID"].intValue
-//                    LostArticleResult().id = json["SearchLostArticleService"]["row"][i]["ID"].intValue
-//                    LostArticleResult().getName = json["SearchLostArticleService"]["row"][i]["GET_NAME"].stringValue
-                    
-//                }
-//                    self.getTakePlace.append(json["SearchLostArticleService"]["row"][i]["TAKE_PLACE"].stringValue)
-//                    self.getName.append(json["SearchLostArticleService"]["row"][i]["GET_NAME"].stringValue)
-//                    //self.getData.append(json["SearchLostArticleService"]["row"][i]["GET_DATA"].stringValue)
-//                    self.getPosition.append(json["SearchLostArticleService"]["row"][i]["GET_POSITION"].stringValue)
-//                }
+
             
             // 통신 실패
             case .failure(let error):
@@ -211,7 +196,7 @@ class ViewController: UIViewController {
         resultArticle = LostArticleType.getEnumFromKoreanType(korean: (articleTextField.text)!).rawValue
         print(resultArticle)
         print(#function, LostArticleType.getEnumFromKoreanType(korean: (articleTextField?.text)!))
-//        selectArticle = ""
+        articleTextField.text = resultArticle
     }
     
     @objc func onPickCancelArticle() {
@@ -222,15 +207,15 @@ class ViewController: UIViewController {
     @objc func onPickDonePlace() {
         placeTextField.text = self.selectPlace.rawValue
         placeTextField.resignFirstResponder()
-        resultPlace = LostPlaceType.getEnumFromKoreanType(korean: (placeTextField.text)!).rawValue
-        print(resultPlace)
-        print(#function, LostPlaceType.getEnumFromKoreanType(korean: (placeTextField.text)!))
-        //selectPlace = ""
+//        resultPlace = LostPlaceType.getEnumFromKoreanType(korean: (placeTextField.text)!).rawValue
+        resultPlace = LostPlaceType.getKoreanFromLostPlaceType(type: LostPlaceType(rawValue: placeTextField.text!)!)
+        print(#function, resultPlace)
+        placeTextField.text = resultPlace
     }
     
     @objc func onPickCancelPlace() {
         placeTextField.resignFirstResponder()
-        //selectPlace = ""
+//        selectPlace = ""
     }
 }
 
@@ -257,13 +242,15 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
         }
     }
     
-//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-//        if pickerView == articlePickerView {
-//            return selectArticle = articlePickerData[row]
-//        }
-//        else {
-//            selectPlace = placePickerData[row]
-//        }
-//    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == articlePickerView {
+            return selectArticle = LostArticleType(rawValue: articlePickerData[row])!
+        }
+        else {
+            print("현재 결과값 : ", LostPlaceType.allEnumKoreanArray()[row])
+            print(type(of: LostPlaceType(rawValue: placePickerData[row])))
+            return selectPlace = LostPlaceType(rawValue: placePickerData[row]) ?? LostPlaceType.subway1_4
+        }
+    }
 }
 
--- Podfile
+++ Podfile
@@ -6,7 +6,7 @@ target 'LostAndFound' do
   use_frameworks!
 
   # Pods for LostAndFound
-  pod 'Alamofire', '~> 5.1'
+  pod 'Alamofire', '~> 5.2'
   pod 'SwiftyJSON', '~> 4.0'
 
 end
